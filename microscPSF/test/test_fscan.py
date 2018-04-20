c5de6313eb158d7180bf54a36d013e2ee1a00dca
Hazen Babcock <hbabcock@mac.com>
2018-04-20 16:58:46 -0400

    Add tests of particle z position and camera position.

--- microscPSF/test/test_fscan.py
+++ microscPSF/test/test_fscan.py
@@ -8,6 +8,9 @@ import microscPSF
 import microscPSF.microscPSF as msPSF
 
 def test_01():
+    """
+    Particle on surface.
+    """
     mp = msPSF.m_params
     rv = numpy.arange(0.0, 1.01, 0.1)
     zv = numpy.arange(-1.0, 1.01, 0.2)
@@ -15,11 +18,40 @@ def test_01():
     fast_rz = msPSF.gLZRFocalScan(mp, rv, zv)
     slow_rz = msPSF.gLZRFocalScanSlow(mp, rv, zv)
 
-    print(numpy.allclose(fast_rz, slow_rz))
+    assert (numpy.allclose(fast_rz, slow_rz))
+
+
+def test_02():
+    """
+    Particle above surface.
+    """
+    mp = msPSF.m_params
+    rv = numpy.arange(0.0, 1.01, 0.1)
+    zv = numpy.arange(-1.0, 1.01, 0.2)
+
+    fast_rz = msPSF.gLZRFocalScan(mp, rv, zv, pz = 0.1)
+    slow_rz = msPSF.gLZRFocalScanSlow(mp, rv, zv, pz = 0.1)
+
+    assert (numpy.allclose(fast_rz, slow_rz, atol = 1.0e-4, rtol = 1.0e-4))
+
     
-    print(numpy.max(numpy.abs(fast_rz - slow_rz)))
+def test_03():
+    """
+    Detector offset.
+    """
+    mp = msPSF.m_params
+    rv = numpy.arange(0.0, 1.01, 0.1)
+    zv = numpy.arange(-1.0, 1.01, 0.2)
 
+    zd = mp["zd0"] + 1000
+    fast_rz = msPSF.gLZRFocalScan(mp, rv, zv, zd = zd)
+    slow_rz = msPSF.gLZRFocalScanSlow(mp, rv, zv, zd = zd)
 
+    assert (numpy.allclose(fast_rz, slow_rz, atol = 1.0e-4, rtol = 1.0e-4))
+
+    
 if (__name__ == "__main__"):
     test_01()
+    test_02()
+    test_03()
     
